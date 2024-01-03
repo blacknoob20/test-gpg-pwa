ff76615a2909bbaf8693cae27f72f8f3e21352c6
cristian.guerrero <cristian.guerrero@guayas.gob.ec>
2024-01-03 16:12:23 -0500

    Agrego el basename path en el router

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('sw.js', { scope: '.' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/test-gpg-pwa/sw.js', { scope: '/test-gpg-pwa/' })})}
